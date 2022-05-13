9dcbc79ce2b1b8a30be849fcf809a27bbc39e3e2
Fadhillah Indrawan <fadhillah19001@mail.unpad.ac.id>
2022-05-13 16:03:07 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtikelController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\ArtikelsController;
 /*
 |--------------------------------------------------------------------------
